5013bad7a5247f3e0378866190fb941753721af4
jennyliang220 <jiaojiaomao220@163.com>
2018-07-09 14:42:33 +0800

    feat: 优化代码

--- components/mip-shell-xiaoshuo/mip-shell-xiaoshuo.js
+++ components/mip-shell-xiaoshuo/mip-shell-xiaoshuo.js
@@ -26,11 +26,6 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
         // this.footerTriggerButton = shellConfig['footer-button-area']
     }
 
-    // 基类方法：初始化。用于除头部bar之外的元素
-    // renderOtherParts () {
-    //     super.renderOtherParts()
-    // }
-
     // 基类方法：绑定头部弹层事件。
     bindHeaderEvents () {
         // todo 针对$el做处理
@@ -43,15 +38,11 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
     _initAllObjects() {
         let configMeta = (this.shellConfig && this.shellConfig.routes[0].meta) || window.MIP.viewer.page.currentPageMeta
         // 创建底部 bar
-        console.log('创建了一个footer')
         this.footer = new Footer(configMeta.footer)
-
         // 创建目录侧边栏
         this.catalog = new Catalog(configMeta.catalog);
-
         // 创建字体调整栏
         this.fontSize = new FontSize(document.querySelector('.mip-shell-footer-wrapper .mip-shell-xiaoshuo-control-fontsize'));
-
         // 创建模式切换（背景色切换）
         this.mode = new Mode();
     }
@@ -61,12 +52,8 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
     bindAllEvents() {
         super.bindAllEvents()
         // 初始化所有内置对象
-        console.log('bindAllEvents')
         let me = this
         let event = window.MIP.util.event
-
-        this.processShellConfig
-
         // 暴露给外部html的调用方法，显示底部控制栏
         // 使用 on="tap:xiaoshuo-shell.showShellFooter"调用
         this.addEventAction('showShellFooter', function () {
@@ -74,24 +61,19 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
                 name: 'showShellFooter'
             })
         })
-
         // 暴露给外部html的调用方法, 显示目录侧边栏
         this.addEventAction('showShellCatalog', function () {
             window.MIP.viewer.page.emitCustomEvent(window.parent, false, {
                 name: 'showShellCatalog'
             })
         })
-
         // 功能绑定：背景色切换
         this.addEventAction('changeMode', function (e, mode) {
             window.MIP.viewer.page.broadcastCustomEvent({
                 name: 'changeMode',
-                data: {
-                    'mode': mode
-                }
+                data: {'mode': mode }
             })
         })
-
         // 绑定底部弹层控制条拖动事件
         this.addEventAction('showFontAdjust', function (e) {
             this.fontSize.showFontBar(e)
@@ -103,7 +85,6 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
         // 绑定弹层点击关闭事件
         if (this.$buttonMask) {
             this.$buttonMask.onclick = () => {
-                console.log('触发shell事件 关闭弹层')
                 // 关闭所有可能弹出的bar
                 me.toggleDOM(me.$buttonWrapper, false) // 关不掉分享按钮组
                 me.$buttonWrapper.style.display = 'none'; // XXX: hack, 修复 toggleDOM 中强制给未展示底部按钮组增加display:block问题
@@ -113,9 +94,7 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
                 // 关闭黑色遮罩
                 me.toggleDOM(me.$buttonMask, false)
             }
-        } else {
-            // debugger
-        }
+        } else {}
     }
 
     // 基类方法：绑定页面可被外界调用的事件。
@@ -123,25 +102,18 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
     bindRootEvents () {
         super.bindRootEvents()
         let me = this
-
         // 绑定 Root shell 字体bar拖动事件
         this.fontSize.bindDragEvent()
-
         // 承接emit事件：根页面修改页面模式、背景
         window.addEventListener('changeMode', (e, data) => {
-            console.log('触发shell事件 修改页面模式、背景')
             me.mode.change(e, e.detail[0].mode)
         })
-
         // 承接emit事件：根页面展示底部控制栏
         window.addEventListener('showShellFooter', (e, data) => {
-            console.log('触发shell事件 展示底部控制栏')
             me.footer.show(me)
         })
-
-        // // 承接emit事件：显示目录侧边栏
+        // 承接emit事件：显示目录侧边栏
         window.addEventListener('showShellCatalog', (e, data) => {
-            console.log('触发shell事件 显示目录侧边栏')
             me.catalog.show(me)
             me.footer.hide()
         })
@@ -157,8 +129,7 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
 
     // todo 干什么的？
     unbindHeaderEvents () {
-        // super.unbindHeaderEvents()
-
+        super.unbindHeaderEvents()
         // if (this.footEventHandler) {
         //     // LJ: 这里为什么还要调用一次？
         //     this.footEventHandler()
@@ -170,7 +141,6 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
     // todo 啥时候用？
     updateOtherParts () {
         console.log('updateOtherParts')
-        // this.$footer.innerHTML = this._renderFooter()
     }
 
     // 基类方法: 处理头部自定义按钮点击事件，由于没有按钮，置空
@@ -184,6 +154,11 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
         //         {"name": "cancel", "text": "取消"}
         //     ]
         // }
+    }
+
+    // 基类方法：初始化。用于除头部bar之外的元素
+    renderOtherParts () {
+        super.renderOtherParts()
     }
 
 }
