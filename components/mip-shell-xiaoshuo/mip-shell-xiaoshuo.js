0e82ed6e7a814aa016acf7dea1b993f7a68c722d
jennyliang220 <jiaojiaomao220@163.com>
2018-07-11 21:05:21 +0800

    feat: 改为线下品专广告

--- components/mip-shell-xiaoshuo/mip-shell-xiaoshuo.js
+++ components/mip-shell-xiaoshuo/mip-shell-xiaoshuo.js
@@ -14,6 +14,8 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
     super(...args)
     this.alwaysReadConfigOnLoad = true
     this.transitionContainsHeader = false
+    // 处理浏览器上下滚动边界，关闭弹性
+    this._scrollBoundary()
   }
 
   // 基类方法：绑定页面可被外界调用的事件。
@@ -173,5 +175,74 @@ export default class MipShellXiaoshuo extends window.MIP.builtinComponents.MipSh
     super.updateOtherParts()
     // 重新渲染footer
     this.footer._render(this.currentPageMeta.footer)
+  }
+
+  /**
+   * 滚动边界处理
+   */
+  _scrollBoundary () {
+    let touchStartEvent
+    let {rect, css} = MIP.util
+    // 收集body child元素 并进行包裹
+    let scrollaBoundaryTouch = document.createElement('div')
+    let offsetHeight
+    let bodyPaddingTop
+    let body = document.body
+    let touchTarget
+    let stopProFun = e => e.stopPropagation()
+
+    scrollaBoundaryTouch.setAttribute('mip-shell-scrollboundary', true);
+    [].slice.call(body.children).forEach(child => {
+      if (/^(SCRIPT|IFRAME|MIP-SHELL-INSERVICE|MIP-DATA)/.test(child.nodeName)) {
+        return
+      }
+      scrollaBoundaryTouch.appendChild(child)
+    })
+    body.appendChild(scrollaBoundaryTouch)
+
+    // 添加事件处理
+    scrollaBoundaryTouch.addEventListener('touchstart', e => {
+      touchStartEvent = e
+      // 内滚 兼容处理
+      touchTarget = this.getClosestScrollElement(e.target)
+      if (touchTarget) {
+        touchTarget.addEventListener('touchmove', stopProFun)
+      }
+    })
+
+    scrollaBoundaryTouch.addEventListener('touchmove', e => {
+      let touchRect = e.targetTouches[0]
+      let startTouchReact = touchStartEvent.targetTouches[0]
+
+      // 兼容模式处理
+      offsetHeight = document.compatMode === 'BackCompat'
+        ? document.body.clientHeight
+        : document.documentElement.clientHeight
+
+      bodyPaddingTop = bodyPaddingTop || parseInt(css(body, 'paddingTop'), 10)
+      let scrollTop = body.scrollTop || rect.getScrollTop()
+      let scrollHeight = rect.getElementRect(scrollaBoundaryTouch).height + bodyPaddingTop
+
+      // 到达顶部时 && 是向下滚动操作
+      // 到达底部时 && 并且 向上滚动操作
+      let isprevent = (
+        touchRect.pageY >= startTouchReact.pageY &&
+          touchRect.clientY > startTouchReact.clientY &&
+          scrollTop < 5) ||
+          (
+            touchRect.pageY < startTouchReact.pageY &&
+            scrollTop + offsetHeight >= scrollHeight
+          )
+      if (isprevent) {
+        e.preventDefault()
+      }
+      e.stopPropagation()
+    })
+
+    scrollaBoundaryTouch.addEventListener('touchend', () => {
+      if (touchTarget) {
+        touchTarget.removeEventListener('touchmove', stopProFun)
+      }
+    })
   }
 }
