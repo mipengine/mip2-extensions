53bb7f45564dfe852cd2ea936259241878a01b18
Espoir-L <413308430@qq.com>
2018-07-19 17:01:10 +0800

    fix：调试QQ浏览器下无法播放视频的问题

--- components/mip-novel-video/mip-novel-video.vue
+++ components/mip-novel-video/mip-novel-video.vue
@@ -120,6 +120,7 @@ export default {
     this.timeExpired()
     this.initVideoIndex()
     isShouldVideo = +customStorage.get(VIDEOINDEX) === 2 || false
+    alert('; isSF:' + isSF + '; version: ' + detector.getMobileSystemVersion() + '; isShouldVideo: ' + customStorage.get(VIDEOINDEX) + '; src:' + this.src + '; tsurl:' + this.tsurl)
     if (this.isShow) {
       console.log('是否SF：' + (isSF || false) + '；页数：' + customStorage.get(VIDEOINDEX))
       this.readContainerNoScroll()
