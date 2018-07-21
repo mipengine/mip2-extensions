cefc47477f993a19ce00298132602537729c7d3e
jennyliang220 <jiaojiaomao220@163.com>
2018-07-21 18:17:37 +0800

    feat: 增加常量配置文件

--- components/mip-shell-xiaoshuo/constant-config.js
+++ components/mip-shell-xiaoshuo/constant-config.js
@@ -2,31 +2,32 @@
  * @file 事件常量名配置
  * @author JennyL, liujing
  */
+
 export const Event = {
-  /** @constant {string} 上一页按钮被点击事件 */
+  /** @member {string} 上一页按钮被点击事件'previous-page-button-click' */
   PREVIOUS_PAGE_BUTTON_CLICK: 'previous-page-button-click',
-  /** @constant {string} 下一页按钮被点击事件 */
+  /** @member {string} 下一页按钮被点击事件'next-page-button-click' */
   NEXT_PAGE_BUTTON_CLICK: 'next-page-button-click',
-  /** @constant {string} 快速切换章节拖动条事件 */
+  /** @member {string} 快速切换章节拖动条事件'fastforward-control-strip-drag' */
   FASTFORWARD_CONTROL_STRIP_DRAG: 'fastforward-control-strip-drag',
-  /** @constant {string} 本页是本章最后一页 */
+  /** @member {string} 本页是本章最后一页'at-chapter-end' */
   AT_CHAPTER_END: 'at-chapter-end',
-  /** @constant {string} 本页是本章第一页 */
+  /** @member {string} 本页是本章第一页'at-chapter-start' */
   AT_CHAPTER_START: 'at-chapter-start',
-  /** @constant {string} 从搜索结果页进入 */
+  /** @member {string} 从搜索结果页进入'is-root-page' */
   IS_ROOT_PAGE: 'is-root-page',
-  /** @constant {string} 目录展开按钮被点击 */
+  /** @member {string} 目录展开按钮被点击'catalog-show' */
   CATALOG_SHOW: 'catalog-show',
-  /** @constant {string} 目录关闭按钮被点击 */
+  /** @member {string} 目录关闭按钮被点击'catalog-hide' */
   CATALOG_HIDE: 'catalog-hide',
-  /** @constant {string} 阅读进度拖动 */
+  /** @member {string} 阅读进度拖动'catalog-drag' */
   CATALOG_DRAG: 'catalog-drag',
-  /** @constant {string} 设置打开按钮被点击 */
+  /** @member {string} 设置打开按钮被点击'show-setting' */
   SHOW_SETTING: 'show-setting',
-  /** @constant {string} 设置关闭按钮被点击 */
+  /** @member {string} 设置关闭按钮被点击'hide-setting' */
   HIDE_SETTING: 'hide-setting',
-  /** @constant {string} 切换背景色 */
+  /** @member {string} 切换背景色'change-theme' */
   CHANGE_THEME: 'change-theme',
-  /** @constant {string} 修改字体大小 */
+  /** @member {string} 修改字体大小'change-font-size' */
   CHANGE_FONT_SIZE: 'change-font-size'
 }
