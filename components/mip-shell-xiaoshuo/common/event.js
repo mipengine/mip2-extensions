428a9246a661b0c351e24d4eef083fe62d17a650
jennyliang220 <jiaojiaomao220@163.com>
2018-07-22 15:00:01 +0800

    feat: 增加阅读器事件和尝试接收

--- components/mip-shell-xiaoshuo/common/event.js
+++ components/mip-shell-xiaoshuo/common/event.js
@@ -4,7 +4,7 @@
  * TODO：
  *     1. catalog数据支持异步获取
  */
-import {constant} from '../constant-config'
+import {Constant} from '../constant-config'
 let event = window.MIP.util.event
 
 class Event {
@@ -17,10 +17,10 @@ class Event {
      * 当【下一页】按钮被点击了, 抛出'next-page-button-clicked'事件
      * @fires 'next-page-button-clicked'
      */
-    let nextPageButton = '.mip-shell-footer .page-next'
+    let nextPageButton = '.mip-shell-footer .page-next:not(.disabled)'
     event.delegate(document.body, nextPageButton, 'click', function () {
       window.MIP.viewer.page.emitCustomEvent(isRootPage ? window : window.parent, false, {
-        name: constant.NEXT_PAGE_BUTTON_CLICK
+        name: Constant.NEXT_PAGE_BUTTON_CLICK
       })
     })
 
@@ -28,15 +28,15 @@ class Event {
      * 当【上一页】按钮被点击了, 抛出'previous-page-button-clicked'事件
      * @fires 'next-page-button-clicked'
      */
-    let previousPageButton = '.mip-shell-footer .page-previous'
+    let previousPageButton = '.mip-shell-footer .page-previous:not(.disabled)'
     event.delegate(document.body, previousPageButton, 'click', function () {
       window.MIP.viewer.page.emitCustomEvent(isRootPage ? window : window.parent, false, {
-        name: constant.PREVIOUS_PAGE_BUTTON_CLICK
+        name: Constant.PREVIOUS_PAGE_BUTTON_CLICK
       })
     })
 
-    window.addEventListener(constant.NEXT_PAGE_BUTTON_CLICK, e => console.log('NEXT_PAGE_BUTTON_CLICK'))
-    window.addEventListener(constant.PREVIOUS_PAGE_BUTTON_CLICK, e => console.log('PREVIOUS_PAGE_BUTTON_CLICK'))
+    window.addEventListener(Constant.NEXT_PAGE_BUTTON_CLICK, e => console.log('NEXT_PAGE_BUTTON_CLICK'))
+    window.addEventListener(Constant.PREVIOUS_PAGE_BUTTON_CLICK, e => console.log('PREVIOUS_PAGE_BUTTON_CLICK'))
   }
 }
 
