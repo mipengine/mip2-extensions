0bea5882201881ce5765c765b3d6c16722a60b6d
huanghuiquan <huanghuiquanhhh@gmail.com>
2018-07-25 12:02:23 +0800

    update code style

--- components/mip-fastclick/mip-fastclick.js
+++ components/mip-fastclick/mip-fastclick.js
@@ -1,8 +1,8 @@
 import FastClick from 'fastclick'
 
 export default class MIPFastClick extends MIP.CustomElement {
-  constructor (ele) {
-    super(ele)
+  constructor (...args) {
+    super(...args)
     let targetId = this.element.getAttribute('target')
     let target = document.querySelector('#' + targetId) || this.element
     FastClick.attach(target)
