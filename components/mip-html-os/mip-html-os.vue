937388fdebb8bc6e6a59dc68eada9551498b39d2
tfciw <1315356284@qq.com>
2018-08-06 15:39:35 +0800

    mip-html-os迁移

---
+++ components/mip-html-os/mip-html-os.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper" v-if="show">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+
+}
+</style>
+
+<script>
+export default {
+  props: {
+    os: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      show: false
+    }
+  },
+  created() {
+    let isOS = false;
+    let osUA = navigator.userAgent.toLowerCase();
+    switch(this.os) {
+      case 'android': isOS
+        isOS = osUA.indexOf('android') > -1;
+        break;
+      case 'ios':
+        isOS = !!osUA.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/i);
+        break;
+    }
+    this.show = isOS;
+  }
+}
+</script>
