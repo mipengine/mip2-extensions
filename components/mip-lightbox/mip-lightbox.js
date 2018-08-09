34a27359f77912e352831cb01288c633308db163
caoru828 <caoru@baidu.com>
2018-08-09 13:27:49 +0800

    feat: mip-lightbox迁移mip2

---
+++ components/mip-lightbox/mip-lightbox.js
@@ -0,0 +1,26 @@
+import './mip-lightbox.less'
+// let {
+//   CustomElement,
+//   util,
+//   fixedElement
+// } = MIP
+// let Gesture = util.Gesture;
+// let scrollTop = {
+//     body: 0,
+//     documentElement: 0,
+//     offset: 0
+// }
+// export default class MipLightbox extends CustomElement {
+//   constructor(...args) {
+//     // 继承父类属性、方法
+//     super(...args)
+//   }
+//   // 提前渲染
+//   prerenderAllowed() {
+//     return true
+//   }
+//   // 插入文档时执行
+//   build() {
+//     util.css(xxxx)
+//   }
+// }
