ab8361f3fb2be70acd40fd4dc54386179c129928
CaoRu <18846432589@163.com>
2018-08-28 13:01:20 +0800

    小说阅读器 feat: 新增左滑目录页收起，上下滑设置收起功能 (#235)
    *  feat: 新增左滑目录页收起，上下滑设置收起功能
    
    *  feat: 修改代码规范
    
    *  优化代码格式
    
    *  测试回归修复

--- components/mip-shell-xiaoshuo/feature/catalog.js
+++ components/mip-shell-xiaoshuo/feature/catalog.js
@@ -6,7 +6,6 @@
  */
 
 let util = MIP.util
-let rect = util.rect
 class Catalog {
   constructor (config, book) {
     // 渲染侧边栏目录元素
@@ -20,10 +19,12 @@ class Catalog {
   _renderCatalog (catalogs, book) {
     let renderCatalog
     let title = ''
+    let chapterStatus = ''
     let chapterNumber = ''
     if (book) {
       title = book.title
       chapterNumber = book.chapterNumber
+      chapterStatus = book.chapterStatus
     }
     let catalogHtml = `
       <div class="mip-catalog-btn book-catalog-info">
@@ -31,7 +32,7 @@ class Catalog {
           <div class="book-catalog-info-title">
             <p class="book-catalog-title-name catalog-title">${title}</p>
             <div class="catalog-content-total-wrapper">
-              <p class="catalog-content-total">${chapterNumber}</p>
+              <p class="catalog-content-total"><span>${chapterStatus}</span><span class="chapter-number">${chapterNumber}</span></p>
             </div>
           </div>
           <div class="catalog-content-center-wrapper">
@@ -50,15 +51,8 @@ class Catalog {
           </div>
           </div>
         </div>
-        <div class="scroll">
-            <div class="catalog-scroll" >
-              <div id="catalog-scroll-btn">
-                <div class="scroll-btn"></div>
-                <div class="scroll-btn"></div>
-                <div class="scroll-btn"></div>
-              </div>
-            </div>
-        </div>
+        <!--<div class="scroll">-->
+        <!--</div>-->
       </div>
     `
     if (!catalogs) {
@@ -91,12 +85,9 @@ class Catalog {
       )
     }
     $catalogSidebar.innerHTML = catalogHtml // 目录页HTML
-    let $wrapper = $catalogSidebar.querySelector('.novel-catalog-content-wrapper')
     let $catalog = $catalogSidebar.querySelector('.mip-shell-catalog')
     let $contentTop = $catalogSidebar.querySelector('.mip-catalog-btn') // 上边元素
-    let $catalogScroll = $catalogSidebar.querySelector('.catalog-scroll') // 滚动条
     let $scroll = $catalogSidebar.querySelector('.scroll') // 滚动条
-    let $catalogButton = $catalogSidebar.querySelector('#catalog-scroll-btn') // 小按钮
     let $catalogContent = $catalogSidebar.querySelector('.novel-catalog-content')
     $catalogContent.innerHTML = renderCatalog(catalogs)
     let $catalogBook = $catalogSidebar.querySelector('.book-catalog-info-title')
@@ -107,23 +98,8 @@ class Catalog {
       $catalog.style.height = 'calc(100% - 62px)'
       $catalog.style.height = '-webkit-calc(100% - 62px)'
     }
-
-    let catalogScroll = {
-      catalogSidebar: $catalogSidebar,
-      catalog: $catalog,
-      catalogScroll: $catalogScroll,
-      catalogButton: $catalogButton,
-      contentTop: $contentTop,
-      wrapper: $wrapper,
-      catalogContent: $catalogContent
-    }
-    // 自定义滚动条，滑目录内容，右边滚动条到相应位置， 这里需要监听scroll完成事件，用settimeout异步队列模拟，解决兼容问题
-    this.catalogScroll(catalogScroll)
-    //  实现滚动条拖拽函数，拖动滚动条，左边滑到相应位置
-    this.catalogDrag(catalogScroll)
     // 实现倒序，点击倒序，目录顺序倒序，倒序字边正序
     this.reverse($contentTop, $catalogContent)
-
     if (!hadCatalog) {
       $catalogSidebar.appendChild($catalog)
       document.body.appendChild($catalogSidebar)
@@ -135,142 +111,6 @@ class Catalog {
     return $catalogSidebar
   }
   /**
-   * 函数说明：解决translateY的兼容问题，多次用到，封装函数
-   *
-   * @param  {Object} $catalogScroll 滚动条
-   * @param  {Object} scrollTop 滚动多高
-   */
-  moveTranslateY ($catalogScroll, scrollTop) {
-    $catalogScroll.style.transform = 'translateY( ' + scrollTop + 'px)'
-    $catalogScroll.style.webkitTransform = 'translateY( ' + scrollTop + 'px)'
-  }
-  /**
-   * 函数说明：自定义滚动条，滑目录内容，右边滚动条到相应位置，这里需要监听scroll完成事件，用settimeout异步队列模拟，解决兼容问题
-   * @param  {object} $catalogSidebar 整个目录页
-   * @param  {object} $catalog        目录页章节
-   * @param  {object} $catalogScroll  滚动条
-   * @param  {object} $catalogButton  滚动条按钮
-   * @param  {object} $contentTop     目录页章节滚动高度
-   * @param  {object} $wrapper         章节内容最外边元素
-   * @param  {object} $catalogContent 目录页内容
-   */
-  // this.catalogDrag($catalogSidebar, $catalog, $catalogScroll, $catalogButton, $contentTop, $wrapper, $catalogContent)
-  catalogScroll (catalogScroll) {
-    let contentTop
-    let setTime = null
-    let scrollNow
-    let $catalog = catalogScroll.catalog
-    let $catalogScroll = catalogScroll.catalogScroll
-    let $catalogButton = catalogScroll.catalogButton
-    let $contentTop = catalogScroll.contentTop
-    let $wrapper = catalogScroll.wrapper
-    let $catalogContent = catalogScroll.catalogContent
-
-    /**
-     * 滑动截止时候让滚动条滚到相应位置
-     *
-     * @param  {number} opacityNum 透明度
-     * @param  {Object} Top 目录页距离顶部高度
-     * @param  {Object} Height 章节以上元素的高度
-     */
-    let scrollToEnd = (opacityNum, Top, Height) => {
-      clearTimeout(setTime)
-      setTime = setTimeout(() => {
-        contentTop = -(rect.getElementOffset($catalogContent).top - rect.getElementOffset($contentTop).height)
-        let clientHeight = rect.getElementOffset($catalog).height - (3 / 2 * rect.getElementOffset($catalogButton).height)
-        let contentHeight = (rect.getElementOffset($catalogContent).height - rect.getElementOffset($catalog).height)
-        let scrollTop = clientHeight * contentTop / contentHeight
-        if (scrollTop >= clientHeight) {
-          scrollTop = clientHeight
-        } else if (scrollTop <= 0) {
-          scrollTop = 0
-        }
-        // 解决translateY的兼容问题，方便后续多次用到， 封装函数
-        this.moveTranslateY($catalogScroll, scrollTop)
-        $catalogScroll.style.opacity = opacityNum
-      }, 100)
-    }
-    $catalogContent.addEventListener('touchstart', (e) => {
-      e.stopPropagation()
-      $catalogScroll.style.opacity = 0
-    })
-    $catalogContent.addEventListener('touchend', (e) => {
-      e.stopPropagation(e)
-      let contentTop = rect.getElementOffset($catalogContent).top // 目录页距离顶部高度
-      let contentHeight = rect.getElementOffset($contentTop).height // 章节以上元素的高度
-      let isTouchEndOver = Math.abs(scrollNow) - Math.abs(contentTop - contentHeight)
-      scrollToEnd(0, contentTop, contentHeight)
-      scrollNow = contentTop
-      if (isTouchEndOver <= 5 && isTouchEndOver >= -5) scrollToEnd(1, contentTop, contentHeight)
-    })
-    $catalogContent.addEventListener('touchmove', (e) => {
-      e.stopPropagation()
-      $catalogScroll.style.opacity = 0
-      scrollNow = (rect.getElementOffset($catalogContent).top - rect.getElementOffset($contentTop).height)
-    })
-    $wrapper.addEventListener('scroll', (e) => {
-      e.stopPropagation()
-      let contentTop = rect.getElementOffset($catalogContent).top
-      let contentHeight = rect.getElementOffset($contentTop).height
-      scrollToEnd(1, contentTop, contentHeight)
-    })
-  }
-  /**
-   * 函数说明：实现滚动条拖拽函数，拖动滚动条，左边滑到相应位置
-   *
-   * @param  {Object} $catalogSidebar  整个目录页
-   * @param  {Object} $catalog         目录页章节
-   * @param  {Object} $catalogScroll   滚动条
-   * @param  {Object} $catalogButton   滚动条按钮
-   * @param  {Object} $contentTop      目录页章节滚动高度
-   * @param  {Object} $wrapper         章节内容最外边元素
-   * @param  {Object} $catalogContent  目录页内容
-   */
-  catalogDrag (catalogScroll) {
-    let $catalog = catalogScroll.catalog
-    let $catalogScroll = catalogScroll.catalogScroll
-    let $catalogButton = catalogScroll.catalogButton
-    let $contentTop = catalogScroll.contentTop
-    let $catalogContent = catalogScroll.catalogContent
-    let touchStartY
-    let contentTop
-    let clientHeight
-    let contentHeight
-    let that = this
-    $catalogScroll.parentNode.addEventListener('touchstart', (e) => {
-      $catalogScroll.style.transition = 'all ease 0'
-      $catalogScroll.style.webkitTransition = 'all ease 0'
-      // 解决浏览器中出现顶部bar出现或消失时页面高度变化导致高度计算不对问题
-      window.onresize = function () {
-        $catalogScroll.style.opacity = 1
-        contentTop = -(rect.getElementOffset($catalogContent).top - rect.getElementOffset($contentTop).height)
-        clientHeight = rect.getElementOffset($catalog).height - (3 / 2 * rect.getElementOffset($catalogButton).height)
-        contentHeight = (rect.getElementOffset($catalogContent).height - rect.getElementOffset($catalog).height)
-      }
-      let touch = e.targetTouches[0]
-      touchStartY = touch.clientY - rect.getElementOffset($catalogScroll).top
-    })
-    $catalogScroll.parentNode.addEventListener('touchmove', (e) => {
-      $catalogScroll.style.transition = 'all ease 0'
-      clientHeight = rect.getElementOffset($catalog).height - (3 / 2 * rect.getElementOffset($catalogButton).height)
-      let touch = e.targetTouches[0]
-      let oTop = touch.clientY - touchStartY
-      contentHeight = (rect.getElementOffset($catalogContent).height - rect.getElementOffset($catalog).height)
-      contentTop = contentHeight * (oTop - rect.getElementOffset($contentTop).height) / clientHeight
-      if (oTop < $contentTop.offsetHeight) {
-        oTop = rect.getElementOffset($contentTop).height
-      } else if ((oTop - $contentTop.offsetHeight) >= clientHeight) {
-        // 解决translateY的兼容问题，多次用到，封装函数
-        that.moveTranslateY($catalogScroll, clientHeight)
-        return
-      }
-      let transY = oTop - $contentTop.offsetHeight
-      // 解决translateY的兼容问题，多次用到，封装函数
-      that.moveTranslateY($catalogScroll, transY)
-      $catalogContent.parentNode.scrollTop = contentTop
-    })
-  }
-  /**
    * 函数说明：实现倒序，点击倒序，目录顺序倒序，倒序字边正序
    *
    * @param  {Object} $contentTop      目录页章节滚动高度
@@ -278,31 +118,45 @@ class Catalog {
    */
   reverse ($contentTop, $catalogContent) {
     let reverse = $contentTop.querySelector('.catalog-reserve')
+    let catalog = $catalogContent.querySelectorAll('div')
+    let reverseName = $contentTop.querySelector('.reverse-name')
+    let temp = []
+    for (let i = 0; i < catalog.length; i++) {
+      temp[i] = catalog[i].outerHTML
+    }
     reverse.addEventListener('click', () => {
-      let catalog = [...$catalogContent.querySelectorAll('div')]
-      let reverseName = $contentTop.querySelector('.reverse-name')
-      if (reverseName.innerHTML === ' 正序 ') {
-        reverseName.innerHTML = ' 倒序 '
-      } else {
-        reverseName.innerHTML = ' 正序 '
-      }
-      let Temp = []
-      for (let i = 0; i < catalog.length; i++) {
-        Temp[i] = catalog[i].innerHTML
-      }
-      for (let i = 0; i < Temp.length; i++) {
-        catalog[i].innerHTML = Temp[Temp.length - 1 - i]
-      }
+      $catalogContent.innerHTML = temp.reverse().join('')
+      reverseName.innerHTML = reverseName.innerHTML === ' 正序' ? ' 倒序' : ' 正序'
     })
   }
-
+  swipeHidden (e, shellElement) {
+    e.preventDefault()
+    this.hide()
+    e.stopPropagation()
+    shellElement.toggleDOM(shellElement.$buttonMask, false)
+  }
+  bindShowEvent (shellElement) {
+    let catalog = document.querySelector('.mip-shell-catalog-wrapper')
+    let swipeLeft = new util.Gesture(document, {
+      preventX: true
+    })
+    let swipeLeftCatalog = new util.Gesture(catalog, {
+      preventX: true
+    })
+    swipeLeft.on('swipeleft', e => {
+      this.swipeHidden(e, shellElement)
+    })
+    // 解决UC浏览器document不滑动问题
+    swipeLeftCatalog.on('swipeleft', e => {
+      this.swipeHidden(e, shellElement)
+    })
+  }
   // 显示侧边目录
   show (shellElement) {
+    this.bindShowEvent(shellElement)
     // XXX: setTimeout用于解决tap执行过早，click执行过晚导致的点击穿透事件
     // window.setTimeout(function () {
     this.$catalogSidebar.classList.add('show')
-    shellElement.toggleDOM(shellElement.$buttonMask, true)
-    this.$catalogSidebar.querySelector('.catalog-scroll').style.opacity = 1
     // 处理UC浏览器默认禁止滑动，触发dom变化后UC允许滑动
     let $catalogContent = this.$catalogSidebar.querySelector('.novel-catalog-content')
     let catalog = [...$catalogContent.querySelectorAll('div')]
