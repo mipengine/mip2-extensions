6846e2e74e2af4d672c8e51848bf6cb2f6680a87
zhangqichao <zhangqichao02@baidu.com>
2018-09-19 09:56:45 +0800

    fix:list

--- components/mip-list/mip-list.js
+++ components/mip-list/mip-list.js
@@ -1,6 +1,6 @@
 /**
  * @file mip-list 组件
- * @author
+ * @author zhangqichao02@baidu.com
  */
 
 let {
@@ -8,7 +8,9 @@ let {
   templates
 } = MIP
 
-let { fetchJsonp } = window
+let {
+  fetchJsonp
+} = window
 
 export default class MipList extends CustomElement {
   constructor (...args) {
@@ -20,11 +22,11 @@ export default class MipList extends CustomElement {
   }
 
   firstInviewCallback () {
-    let { element } = this
+    let {
+      element
+    } = this
     this.container = document.createElement('div')
-    this.applyFillContent(this.container)
     element.appendChild(this.container)
-
     if (!this.container.hasAttribute('role')) {
       this.container.setAttribute('role', 'list')
     }
@@ -33,7 +35,7 @@ export default class MipList extends CustomElement {
     if (element.hasAttribute('synchronous-data')) {
       let script = element.querySelector('script[type="application/json"]')
       let data = script ? JSON.parse(script.textContent.toString()) : null
-      renderTemplate.call(this, data)
+      this.renderTemplate(data)
       return
     }
 
@@ -41,48 +43,88 @@ export default class MipList extends CustomElement {
     let url = this.src
     if (!this.src) {
       console.error('mip-list 的 src 属性不能为空')
+      return
     }
 
     // 有查看更多属性的情况
     if (element.hasAttribute('has-more')) {
       this.addEventAction('more', function () {
-        pushResult.call(this, this.src)
+        this.pushResult(this.src)
       })
     }
 
     if (element.hasAttribute('preLoad')) {
-      let self = this
       url = getUrl(this.src, this.pnName, this.pn++)
       fetchJsonp(url, {
         jsonpCallback: 'callback',
         timeout: this.timeout
-      }).then(function (res) {
+      }).then(res => {
         return res.json()
-      }).then(function (data) {
+      }).then(data => {
         if (!data.status && data.data) {
-          renderTemplate.call(self, data.data)
+          this.renderTemplate(data.data)
         }
       })
     }
   }
+
+  /**
+   * renderTemplate 获取模版
+   *
+   * @param {Object} data 渲染数据
+   */
+  renderTemplate (data) {
+    if (data && data.items && data.items instanceof Array) {
+      templates.render(
+        this.element, data.items
+      ).then(render.bind(this))
+    } else {
+      console.error('数据不符合规范')
+    }
+  }
+
+  /**
+   * pushResult push结果函数
+   *
+   * @param {string} src ajax请求的url
+   */
+  pushResult (src) {
+    if (this.isEnd) {
+      return
+    }
+    this.button = document.querySelector('.mip-list-more')
+    this.button.innerHTML = '加载中...'
+    let url = getUrl(src, this.pnName, this.pn++)
+    fetchJsonp(url, {
+      jsonpCallback: 'callback',
+      timeout: this.timeout
+    }).then(res => {
+      return res.json()
+    }).then(data => {
+      if (!data.status && data.data) {
+        this.renderTemplate(data.data)
+        this.button.innerHTML = '点击查看更多'
+        if (data.data.isEnd) {
+          this.isEnd = data.isEnd
+          this.button.innerHTML = '已经加载完毕'
+          this.button.removeAttribute('on')
+        }
+      } else {
+        this.button.innerHTML = '加载失败'
+      }
+    })
+  }
 }
 
 /**
-  * [getUrl 获取最后拼接好的数据请求 url]
-  *
-  * @param   {string}  src    原始 url
-  * @param   {string}  pnName 翻页字段名
-  * @param   {number}  pn     页码
-  * @returns {string}         拼接好的 url
-*/
+ * getUrl 获取最后拼接好的数据请求 url
+ *
+ * @param {string} src 原始 url
+ * @param {string} pnName 翻页字段名
+ * @param {number} pn 页码
+ * @returns {string} 拼接好的 url
+ */
 function getUrl (src, pnName, pn) {
-  if (!src) {
-    console.error('mip-list 的 src 属性不能为空')
-    return
-  }
-  if (!pnName || !pn) {
-    return
-  }
   let url = src
   if (src.indexOf('?') > 0) {
     url += src[src.length - 1] === '?' ? '' : '&'
@@ -90,71 +132,21 @@ function getUrl (src, pnName, pn) {
   } else {
     url += '?' + pnName + '=' + pn
   }
-
   return url
 }
 
 /**
-  * [renderTemplate 获取模板]
-  *
-  * @param  {Object} data 渲染数据
-*/
-function renderTemplate (data) {
-  if (data && data.items && data.items instanceof Array) {
-    templates.render(
-      this.element, data.items
-    ).then(render.bind(this))
-  } else {
-    console.error('数据不符合规范')
-  }
-}
-
-/**
-  * [render dom渲染函数]
-  *
-  * @param  {Array} htmls [html对象数组]
-*/
+ * render dom渲染函数
+ *
+ * @param {Array} htmls html对象数组
+ */
 function render (htmls) {
   let fragment = document.createElement('div')
-  htmls.map(function (html) {
+  for (let html of htmls) {
     let node = document.createElement('div')
     node.innerHTML = html
     node.setAttribute('role', 'listitem')
-
     fragment.appendChild(node)
-  })
-  this.container.appendChild(fragment)
-}
-
-/**
-  * [pushResult push结果函数]
-  *
-  * @param  {string} src ajax请求的url
-*/
-function pushResult (src) {
-  let self = this
-  if (self.isEnd) {
-    return
   }
-  self.button = document.querySelector('.mip-list-more')
-  self.button.innerHTML = '加载中...'
-  let url = getUrl(src, this.pnName, this.pn++)
-  fetchJsonp(url, {
-    jsonpCallback: 'callback',
-    timeout: this.timeout
-  }).then(function (res) {
-    return res.json()
-  }).then(function (data) {
-    if (!data.status && data.data) {
-      renderTemplate.call(self, data.data)
-      self.button.innerHTML = '点击查看更多'
-      if (data.data.isEnd) {
-        self.isEnd = data.isEnd
-        self.button.innerHTML = '已经加载完毕'
-        self.button.removeAttribute('on')
-      }
-    } else {
-      self.button.innerHTML = '加载失败'
-    }
-  })
+  this.container.appendChild(fragment)
 }
