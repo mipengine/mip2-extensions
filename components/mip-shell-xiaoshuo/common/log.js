ec4c293e00cc1305a4ca07238bdbd4fa44682ce6
guoshuang03 <guoshuang03@baidu.com>
2018-09-26 13:20:13 +0800

    cr修改

--- components/mip-shell-xiaoshuo/common/log.js
+++ components/mip-shell-xiaoshuo/common/log.js
@@ -2,14 +2,14 @@
  * file: 小说 shell 统一日志发送
  * author: JennyL <jiaojiaomao220@163.com>
  */
+
 /**
-   * 发送日志
-   *
-   * @param {string} type 日志类型
-   * @param {Object} info 日志信息
-   */
+ * 发送日志
+ * @param {string} type 日志类型
+ * @param {Object} info 日志信息
+ */
 export function sendWebbLog (type, info) {
-  let data = Object.assign({info: info}, {
+  let data = Object.assign({info}, {
     dim: {
       from: 'novel'
     }
