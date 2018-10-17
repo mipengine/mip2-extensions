ec871d6d9176aa41e796ff0b039e35c1673357d8
sekiyika <pengxing5501854@gmail.com>
2018-10-17 13:32:04 +0800

    完成视差滚动组件

--- components/mip-fx-flying-carpet/mip-fx-flying-carpet.js
+++ components/mip-fx-flying-carpet/mip-fx-flying-carpet.js
@@ -6,37 +6,13 @@
 import './mip-fx-flying-carpet.less'
 
 export default class MIPFxFlyingCarpet extends MIP.CustomElement {
-  /**
-   * 视差滚动组件
-   *
-   * @param {Array.<*>} args args
-   */
-  constructor (...args) {
-    super(...args)
-
-    /**
-     * 组件容器
-     * @type {HTMLElement}
-     * @private
-     */
-    this.container_ = null
-
-    /**
-     * 子节点列表
-     * @type {Array.<HTMLElmeent>}
-     * @private
-     */
-    this.children_ = []
-  }
-
   /** @override */
   build () {
     let container = document.createElement('div')
     container.setAttribute('class', 'i-mip-fx-flying-carpet-container')
-    this.container_ = container
 
-    let childNodes = Array.from(this.element.children)
-    childNodes.forEach(node => container.appendChild(node))
+    let children = Array.from(this.element.children)
+    children.forEach(child => container.appendChild(child))
 
     let clip = document.createElement('div')
     clip.setAttribute('class', 'i-mip-fx-flying-carpet-clip')
