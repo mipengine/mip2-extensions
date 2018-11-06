1db0564577112da633618ba863d0eed7b37fc1e1
tfciw <1315356284@qq.com>
2018-11-06 15:16:28 +0800

    turun

--- components/mip-shell-xiaoshuo/mip-shell-xiaoshuo.js
+++ components/mip-shell-xiaoshuo/mip-shell-xiaoshuo.js
@@ -340,13 +340,14 @@ export default class MipShellXiaoshuo extends MIP.builtinComponents.MipShell {
     })
     logDom.classList.add('logDom')
     document.body.appendChild(logDom)
-    let oscript = document.createElement('script')
-    oscript.src = 'https://cdn.bootcss.com/vConsole/3.2.0/vconsole.min.js'
-    document.body.appendChild(oscript)
     window.addEventListener('showShellFooter', (e, data) => {
-      let footerObject = Object.getOwnPropertyNames(Object.getPrototypeOf(this.footer))
-      for (const i in footerObject) {
-        logDom.innerHTML += footerObject[i] + '</br>'
+      try {
+        let footerObject = Object.getOwnPropertyNames(Object.getPrototypeOf(this.footer))
+        for (const i in footerObject) {
+          logDom.innerHTML += footerObject[i] + '</br>'
+        }
+      } catch (error) {
+        logDom.innerHTML = 'this.footer is not defined'
       }
       this.footer.show(this)
       this.header.show()
