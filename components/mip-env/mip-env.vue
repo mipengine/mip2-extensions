dfe17f591ff325b64e27b6eee63e87d27cc02145
郑大伟 <zhengdawei.zdw@alibaba-inc.com>
2018-11-19 10:08:50 +0800

    style: 修复eslint报错

--- components/mip-env/mip-env.vue
+++ components/mip-env/mip-env.vue
@@ -1,16 +1,21 @@
 <template>
   <div v-if="toggleShow">
-    <slot></slot>
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
-  props: ['scope'],
+  props: {
+    scope: {
+      type: String,
+      default: ''
+    }
+  },
   data: function () {
     return {
       toggleShow: false
-    };
+    }
   },
   methods: {
     cacheOk: function (cache) {
@@ -23,10 +28,10 @@ export default {
           '.bdstatic.com',
           '.mipcdn.com'
         ]
-      };
-      const allowCacheArr = allowCache[cache] || [];
+      }
+      const allowCacheArr = allowCache[cache] || []
       return allowCacheArr.some((item) => {
-        return location.hostname.lastIndexOf(item) != -1;
+        return location.hostname.lastIndexOf(item) !== -1
       })
     },
     dpOk: function (dp) {
@@ -39,128 +44,128 @@ export default {
           '.bdstatic.com',
           '.mipcdn.com'
         ]
-      };
-      if (!MIP.viewer.isIframed) {
-        console.log('not in iframe');
-        return false;
       }
-      const allowDpArr = allowDp[dp] || [];
+      if (!MIP.viewer.isIframed) {
+        console.log('not in iframe')
+        return false
+      }
+      const allowDpArr = allowDp[dp] || []
       return allowDpArr.some((item) => {
-        return location.hostname.lastIndexOf(item) != -1;
+        return location.hostname.lastIndexOf(item) !== -1
       })
     },
     uaOk: function (ua) {
-      const platform = MIP.util.platform;
-      switch(ua) {
+      const platform = MIP.util.platform
+      switch (ua) {
         case 'baidu':
           if (platform.isBaidu()) {
-            return true;
+            return true
           }
-          break;
+          break
         case 'uc':
           if (platform.isUc()) {
-            return true;
+            return true
           }
-          break;
+          break
         case 'chrome':
           if (platform.isChrome()) {
-            return true;
+            return true
           }
-          break;
+          break
         case 'safari':
           if (platform.isSafari()) {
-            return true;
+            return true
           }
-          break;
+          break
         case 'qq':
           if (platform.isQQ()) {
-            return true;
+            return true
           }
-          break;
+          break
         case 'firefox':
           if (platform.isFireFox()) {
-            return true;
+            return true
           }
-          break;
+          break
         default:
-          return false;
+          return false
       }
     },
     osOk: function (os) {
-      switch(os) {
+      switch (os) {
         case 'ios':
           if (MIP.util.platform.isIos()) {
-            return true;
+            return true
           }
-          break;
+          break
         case 'android':
           if (MIP.util.platform.isAndroid()) {
-            return true;
+            return true
           }
-          break;
+          break
         default:
-          return false;
+          return false
       }
     },
     jsonParse: function (jsonStr) {
       try {
-        return MIP.util.jsonParse(jsonStr);
-      }catch(e) {
-        return jsonStr;
+        return MIP.util.jsonParse(jsonStr)
+      } catch (e) {
+        return jsonStr
       }
     },
     scopeOk: function (scope) {
       if (scope) {
-        const scopeJson = this.jsonParse(scope); 
-        if (MIP.util.fn.isPlainObject(scopeJson) && Object.keys(scopeJson).length != 0) {
-          for(const info in scopeJson) {
-            const param = String(scopeJson[info]).toLowerCase();
+        const scopeJson = this.jsonParse(scope)
+        if (MIP.util.fn.isPlainObject(scopeJson) && Object.keys(scopeJson).length !== 0) {
+          for (const info in scopeJson) {
+            const param = String(scopeJson[info]).toLowerCase()
             switch (info) {
               case 'cache':
                 if (!this.cacheOk(param)) {
-                  console.log('cache error');
-                  return false;
+                  console.log('cache error')
+                  return false
                 }
-                break;
+                break
               case 'dp':
                 if (!this.dpOk(param)) {
-                  console.log('dp error');
-                  return false;
+                  console.log('dp error')
+                  return false
                 }
-                break;
+                break
               case 'ua':
                 if (!this.uaOk(param)) {
-                  console.log('ua error');
-                  return false;
+                  console.log('ua error')
+                  return false
                 }
-                break;
+                break
               case 'os':
                 if (!this.osOk(param)) {
-                  console.log('os error');
-                  return false;
+                  console.log('os error')
+                  return false
                 }
-                break;
+                break
               default:
-                console.log('params error');
-                return false;
+                console.log('params error')
+                return false
             }
           }
           console.log('all right')
-          return true;
+          return true
         } else {
-          console.log('scope error');
-          return false;
+          console.log('scope error')
+          return false
         }
       } else {
-        console.log('no scope');
-        return false;
+        console.log('no scope')
+        return false
       }
-    },
+    }
   },
   firstInviewCallback () {
-    const isOk = this.scopeOk(this.scope);
+    const isOk = this.scopeOk(this.scope)
     if (isOk) {
-      this.toggleShow = true;
+      this.toggleShow = true
     }
   }
 }
