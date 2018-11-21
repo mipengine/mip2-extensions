63513c85669aef4fa3b842ea4846bf28ec7dd82c
clark-t <clarktanglei@163.com>
2018-11-21 11:24:19 +0800

    增加组件

---
+++ components/mip-animation/play-state.js
@@ -0,0 +1,12 @@
+/**
+ * @file play-state.js
+ * @author clark-t (clarktanglei@163.com)
+ */
+
+export default {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RUNNING: 'running',
+  PAUSED: 'paused',
+  FINISHED: 'finished'
+}
