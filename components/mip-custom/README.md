3c95663226099ef29313fe5bb869d56ad18e7677
Espoir-L <413308430@qq.com>
2018-11-29 13:55:49 +0800

    feat：搜索合作页迁移MIP2——基本功能的迁移和整体结构的设计

---
+++ components/mip-custom/README.md
@@ -0,0 +1,20 @@
+# `mip-custom`
+
+标题|内容
+----|----
+类型|
+支持布局|
+所需脚本| [https://c.mipcdn.com/static/v2/mip-custom/mip-custom.js<br/>
+https://c.mipcdn.com/static/v1/mip-mustache/mip-mustache.js](https://c.mipcdn.com/static/v2/mip-custom/mip-custom.js)
+
+## 说明
+
+组件功能说明
+
+## 示例
+
+示例说明
+
+## 属性
+
+属性说明
--- components/mip-tabs/mip-tabs.vue
+++ components/mip-tabs/mip-tabs.vue
@@ -271,10 +271,8 @@ export default {
       height: 2px;
       background-color: #409eff;
       z-index: 2;
-      // transition: transform 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
       transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
       list-style: none;
-      // transition: all 0.3s;
     }
   }
 
