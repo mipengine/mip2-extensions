5e10dcf07212a81cfdde4b4c32be3af147e65695
guoshuang03 <guoshuang03@baidu.com>
2018-12-11 19:06:34 +0800

    uezoucha

--- components/mip-shell-xiaoshuo/common/flag.js
+++ components/mip-shell-xiaoshuo/common/flag.js
@@ -62,7 +62,7 @@ class Flag {
     }
     let bkid = getParamFromString(url, 'bkid')
     // 命中bkid，走无限下拉
-    if (this.bkid.indexOf(bkid) > -1 && this.isSids()) {
+    if (this.bkid.indexOf(bkid) > -1) {
       return true
     }
     return false
