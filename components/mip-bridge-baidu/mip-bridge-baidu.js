e7be3efc4eab25eba9c97057bb050f02e1f9c554
yenshih <yenshih95@gmail.com>
2019-01-14 17:29:03 +0800

    feat: Add mip-bridge-baidu

---
+++ components/mip-bridge-baidu/mip-bridge-baidu.js
@@ -0,0 +1,45 @@
+const {CustomElement} = MIP
+
+export default class MipBridgeBaidu extends CustomElement {
+  build () {
+    const url = this.getBridgeScriptUrl()
+
+    if (!url) {
+      return
+    }
+
+    /** @type {HTMLElement} */
+    const element = this.element
+    const script = document.createElement('script')
+
+    script.src = url
+
+    element.appendChild(script)
+  }
+
+  /**
+   * @returns {string} script url of baidu commerial bridge.
+   */
+  getBridgeScriptUrl () {
+    /** @type {HTMLElement} */
+    const element = this.element
+    let siteId = element.getAttribute('site-id')
+
+    if (element.hasAttribute('siteid')) {
+      siteId = siteId || element.getAttribute('siteid')
+      console.warn('[mip-bridge-baidu] siteId 属性将被废弃，请使用 site-id 代替。')
+    }
+
+    if (siteId) {
+      return `//p.qiao.baidu.com/cps/b.js?siteId=${encodeURIComponent(siteId)}`
+    }
+
+    const token = element.getAttribute('token')
+
+    if (token) {
+      return `//hm.baidu.com/hm.js?${encodeURIComponent(token)}`
+    }
+
+    return ''
+  }
+}
