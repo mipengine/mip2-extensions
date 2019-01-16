bd40ce2075075f4f438d5f3497cfd59fe930555d
木森焱 <wangwenxiao03@163.com>
2019-01-16 11:31:22 +0800

    mip-list

--- components/mip-list/mip-list.js
+++ components/mip-list/mip-list.js
@@ -123,27 +123,26 @@ export default class MipList extends CustomElement {
    */
   addClassList(){
     this.addEventAction('class', function (e) {
-      const url = e.target.getAttribute('[class-url]') || ''    
+      const url = e.target.getAttribute('[class-url]') || ''  
       if (url) {
-          fetch(url,{method:'GET', credentials: 'include'})
-          .then(function(res){ 
-              return res.ok ? res.json() : console.error('数据获取失败')
-          })
-          .then(data => {
-              if (data) {
-                  this.add = true
-                  this.renderTemplate(data)
-              } else { 
-                console.log('响应数据为空')
-              }     
-          })
+        fetch(url, {'method':'GET', 'credentials': 'include'})
+        .then(function(res){
+            return res.ok ? res.json() : console.error('数据获取失败')
+        })
+        .then(data => {
+            if (data) {
+                this.add = true
+                this.renderTemplate(data)
+            } else {
+              console.log('响应数据为空')
+            }
+        })
       } else {
         console.error('请求分类url不能为空')
     }
     })
   }
 }
-
 /**
  * getUrl 获取最后拼接好的数据请求 url
  *
