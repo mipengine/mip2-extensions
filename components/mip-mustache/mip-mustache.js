08710729c9764913b3897270e4f69777983eb244
craigchencc <chenyongle@baidu.com>
2019-01-17 10:12:06 +0800

    add details in readme

--- components/mip-mustache/mip-mustache.js
+++ components/mip-mustache/mip-mustache.js
@@ -3,7 +3,7 @@
  * @author chenyongle(chenyongle@baidu.com)
  */
 import sanitizer from './sanitizer'
-import mustache from './mustache'
+import mustache from 'mustache'
 const {templates} = MIP
 let Inheritor = templates.inheritTemplate()
 // 继承自 Template，必须暴露两个方法
