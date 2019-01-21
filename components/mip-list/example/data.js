1c754f16dd6ac236eb13d18216c35cb82aae9a58
Xing PENG <pengxing5501854@gmail.com>
2019-01-21 19:35:52 +0800

    修复不支持 let 机器上 example 报错的bug (#473)

--- components/mip-list/example/data.js
+++ components/mip-list/example/data.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 // 这段逻辑仅供 MIP 官方组件测试使用，站长开发时禁止使用
 (function () {
-  let fn = ''
-  let keys = Object.keys(window)
-  for (let i = 0; i < keys.length; i++) {
+  var fn = ''
+  var keys = Object.keys(window)
+  for (var i = 0; i < keys.length; i++) {
     if (keys[i].indexOf('jsonp_') === 0) {
       fn = keys[i]
       break
