bda7e4f8322374e0353b9c5b021dfe1da557ce01
leizhen <leihzen@gmail.com>
2019-02-26 10:00:19 +0800

    geshi3

--- components/mip-go-history/mip-go-history.js
+++ components/mip-go-history/mip-go-history.js
@@ -1,7 +1,7 @@
 import './index.less'
 
 export default class MIPGoHistory extends MIP.CustomElement {
-  build() {
+  build () {
     let node = this.element
     let address = node.getAttribute('data')
     node.addEventListener('click', () => {
@@ -19,4 +19,3 @@ export default class MIPGoHistory extends MIP.CustomElement {
     })
   }
 }
-
