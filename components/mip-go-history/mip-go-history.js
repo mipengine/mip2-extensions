2a6648754d15bf64bef8459c34756e83d34fa2b6
leizhen <leihzen@gmail.com>
2019-02-26 09:56:08 +0800

    geshi2

--- components/mip-go-history/mip-go-history.js
+++ components/mip-go-history/mip-go-history.js
@@ -1,21 +1,22 @@
 import './index.less'
 
 export default class MIPGoHistory extends MIP.CustomElement {
-  build () {
-    let node=this.element
-    let address=node.getAttribute('data')
-    node.addEventListener('click',()=>{
-      let referrer = document.referrer;
-        let state = window.history.state
-        if(state && state!=null){
-            window.history.back()
-        }else{
-            if(referrer){
-                window.history.back()
-            }else{
-                window.location.href = address
-            }
+  build() {
+    let node = this.element
+    let address = node.getAttribute('data')
+    node.addEventListener('click', () => {
+      let referrer = document.referrer
+      let state = window.history.state
+      if (state && state != null) {
+        window.history.back()
+      } else {
+        if (referrer) {
+          window.history.back()
+        } else {
+
         }
-    });
+      }
+    })
   }
 }
+
