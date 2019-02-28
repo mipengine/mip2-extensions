46da3debde09ce16698543dd9ac4e6a52e9831d0
yenshih <yenshih95@gmail.com>
2019-02-28 11:22:01 +0800

    fix: 修复 service 调用方式

--- components/mip-audio/mip-audio.js
+++ components/mip-audio/mip-audio.js
@@ -120,7 +120,7 @@ export default class MipAudio extends CustomElement {
       isFinite(audio.duration) && // 部分安卓机器 audio.duration 为 Infinite
       audio.duration > 0.1 // 在安卓UC上获取的duration为0.1
 
-    Services.timerFor(window)
+    Services.timer()
       .poll(isValid, 200)
       .then(() => (time.innerHTML = this.msToDate(audio.duration)))
   }
