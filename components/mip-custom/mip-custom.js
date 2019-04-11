dc3ce015fd07d1606640fa4a7f1e0f271f6bc011
liuxiaoning03 <liuxiaoning03@baidu.com>
2019-04-11 14:50:28 +0800

    放开百科名医

--- components/mip-custom/mip-custom.js
+++ components/mip-custom/mip-custom.js
@@ -337,9 +337,9 @@ export default class MipCustom extends CustomElement {
         // 春雨医生
         'm.chunyuyisheng.com',
         // 好大夫
-        'mip.haodf.com'
+        'mip.haodf.com',
         // 百科名医
-        // 'm.baikemy.com'
+        'm.baikemy.com'
       ]
       for (let i = 0; i < specialLink.length; i++) {
         if (commonData.originalUrl && commonData.originalUrl.indexOf(specialLink[i]) > -1) {
