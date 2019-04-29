6581ca1c6438d3c9f8d49b15eacecc32c038bf4f
18369190717pan <270687594@qq.com>
2019-04-29 20:36:03 +0800

    feat: 新增 mip-date-display 组件

---
+++ components/mip-date-display/README.md
@@ -0,0 +1,138 @@
+# mip-date-display 组件
+
+时间显示组件，支持不同地区语言、格式和时区
+
+标题|内容
+----|----
+类型|通用
+支持布局|responsive, fixed-height, fill, container, fixed
+所需脚本| https://c.mipcdn.com/static/v2/mip-mustache/mip-mustache.js<br>https://c.mipcdn.com/static/v2/mip-date-display/mip-date-display.js
+
+`mip-date-display` 组件用于在 MIP 页面中显示不同格式的时间。只需要在组件的标签上配置特定的时间属性，组件将会计算出一系列的时间值，配合 `mip-mustache` 组件，就可以把这些值按照特定的需求渲染出来。如下示例：
+
+## 示例
+
+```html
+  <mip-date-display datetime="2019-04-29T11:33:03.214Z">
+    <template type="mip-mustache">
+      <div>
+        year:{{year}}; month:{{month}}; monthName:{{monthName}}; day:{{day}}; dayName:{{dayName}}; dayNameShort:{{dayNameShort}}; hour:{{hour}}; minute:{{minute}}; second:{{second}}; iso:{{iso}}; yearTwoDigit:{{yearTwoDigit}}; monthTwoDigit:{{monthTwoDigit}}; dayTwoDigit:{{dayTwoDigit}}; hourTwoDigit:{{hourTwoDigit}}; hour12:{{hour12}}; minuteTwoDigit:{{minuteTwoDigit}}; secondTwoDigit:{{secondTwoDigit}}; dayPeriod:{{dayPeriod}};
+      </div>
+    </template>
+  </mip-date-display>
+```
+
+将显示为：
+```
+year:2019; month:4; monthName:四月; day:29; dayName:星期一; dayNameShort:周一; hour:19; minute:33; second:3; iso:2019-04-29T11:33:03.214Z; yearTwoDigit:19; monthTwoDigit:04; dayTwoDigit:29; hourTwoDigit:19; hour12:7; minuteTwoDigit:33; secondTwoDigit:03; dayPeriod:pm;
+```
+## 时间格式
+
+格式 | 输出示例 | 备注
+-- | -- | -
+{year}-{month}-{day} {hour}:{minute}:{second} | 2019-4-29 19:33:3 | -
+{yearTwoDigit}-{monthTwoDigit}-{dayTwoDigit} {hourTwoDigit}:{minuteTwoDigit}:{secondTwoDigit} | 2019-04-29 19:33:03 | -
+{monthName} {dayName} {dayNameShort} | 四月 星期一 周一 | `locale='zh-cn'`
+{iso} | 2019-04-29T11:33:03.214Z | -
+{hour12} {dayPeriod}| 7 pm | -
+
+## 属性
+
+用来指定目标时间的参数：`datetime`, `timestamp-ms`, `timestamp-seconds`, 至少需要其中一个。
+
+### datetime
+
+ISO 格式的时间，如 `2019-04-29T11:33:03.214Z`
+
+### timestamp-ms
+
+目标时间戳（ms），如 `1556537592614`
+
+### timestamp-seconds
+
+目标时间戳（s），如 `1556537592`
+
+### offset-seconds (可选)
+
+距离目标时间的时间偏移量（s），可以是正数或负数，如 `offset-seconds="60"` 表示将目标时间增加 60 秒
+
+### display-in (可选)
+
+如果设置为值utc，则display-in属性会将给定日期转换为UTC。
+
+### locale (可选)
+
+设置时间单位显示的语言，默认值 `zh-cn`（简体中文）
+
+locale 值 | 语言
+-- | --
+de | German
+en | English
+es | Spanish
+fr | French
+id | Indonesian
+it | Italian
+ja | Japanese
+ko | Korean
+nl | Dutch
+pt | Portuguese
+ru | Russian
+th | Thai
+tr | Turkish
+vi | Vietnamese
+zh-cn | 简体中文
+zh-tw | Chinese Traditional
+
+## 示例
+
+```html
+    <!-- display with datetime -->
+  <mip-date-display datetime="2019-04-29T11:33:03.214Z">
+    <h2>Date: 2019-04-29T15:05:05.000Z</h2>
+    <template type="mip-mustache">
+       <div>
+        year:{{year}}; month:{{month}}; monthName:{{monthName}}; day:{{day}}; dayName:{{dayName}}; dayNameShort:{{dayNameShort}}; hour:{{hour}}; minute:{{minute}}; second:{{second}}; iso:{{iso}}; yearTwoDigit:{{yearTwoDigit}}; monthTwoDigit:{{monthTwoDigit}}; dayTwoDigit:{{dayTwoDigit}}; hourTwoDigit:{{hourTwoDigit}}; hour12:{{hour12}}; minuteTwoDigit:{{minuteTwoDigit}}; secondTwoDigit:{{secondTwoDigit}}; dayPeriod:{{dayPeriod}};
+      </div>
+    </template>
+  </mip-date-display>
+
+  <!-- display in utc -->
+  <mip-date-display datetime="2019-04-29T11:33:03.214Z" display-in="utc">
+      <h2>Date: 2019-04-29T15:05:05.000Z</h2>
+      <template type="mip-mustache">
+         <div>
+        year:{{year}}; month:{{month}}; monthName:{{monthName}}; day:{{day}}; dayName:{{dayName}}; dayNameShort:{{dayNameShort}}; hour:{{hour}}; minute:{{minute}}; second:{{second}}; iso:{{iso}}; yearTwoDigit:{{yearTwoDigit}}; monthTwoDigit:{{monthTwoDigit}}; dayTwoDigit:{{dayTwoDigit}}; hourTwoDigit:{{hourTwoDigit}}; hour12:{{hour12}}; minuteTwoDigit:{{minuteTwoDigit}}; secondTwoDigit:{{secondTwoDigit}}; dayPeriod:{{dayPeriod}};
+      </div>
+      </template>
+  </mip-date-display>
+
+  <!-- display with timestamp-ms -->
+  <mip-date-display timestamp-ms="1556537592614">
+      <h2>timestamp-ms: 1556537593214</h2>
+      <template type="mip-mustache">
+         <div>
+        year:{{year}}; month:{{month}}; monthName:{{monthName}}; day:{{day}}; dayName:{{dayName}}; dayNameShort:{{dayNameShort}}; hour:{{hour}}; minute:{{minute}}; second:{{second}}; iso:{{iso}}; yearTwoDigit:{{yearTwoDigit}}; monthTwoDigit:{{monthTwoDigit}}; dayTwoDigit:{{dayTwoDigit}}; hourTwoDigit:{{hourTwoDigit}}; hour12:{{hour12}}; minuteTwoDigit:{{minuteTwoDigit}}; secondTwoDigit:{{secondTwoDigit}}; dayPeriod:{{dayPeriod}};
+      </div>
+      </template>
+  </mip-date-display>
+
+  <!-- display with timestamp-seconds -->
+  <mip-date-display timestamp-seconds="1556537592">
+      <h2>timestamp-seconds: 1556537593</h2>
+      <template type="mip-mustache">
+         <div>
+        year:{{year}}; month:{{month}}; monthName:{{monthName}}; day:{{day}}; dayName:{{dayName}}; dayNameShort:{{dayNameShort}}; hour:{{hour}}; minute:{{minute}}; second:{{second}}; iso:{{iso}}; yearTwoDigit:{{yearTwoDigit}}; monthTwoDigit:{{monthTwoDigit}}; dayTwoDigit:{{dayTwoDigit}}; hourTwoDigit:{{hourTwoDigit}}; hour12:{{hour12}}; minuteTwoDigit:{{minuteTwoDigit}}; secondTwoDigit:{{secondTwoDigit}}; dayPeriod:{{dayPeriod}};
+      </div>
+      </template>
+  </mip-date-display>
+
+  <!-- display with timestamp-seconds and offset-seconds -->
+  <mip-date-display timestamp-seconds="1556537592" offset-seconds="600">
+      <h2>timestamp-seconds: 1556537593; offset-seconds: 600</h2>
+      <template type="mip-mustache">
+         <div>
+        year:{{year}}; month:{{month}}; monthName:{{monthName}}; day:{{day}}; dayName:{{dayName}}; dayNameShort:{{dayNameShort}}; hour:{{hour}}; minute:{{minute}}; second:{{second}}; iso:{{iso}}; yearTwoDigit:{{yearTwoDigit}}; monthTwoDigit:{{monthTwoDigit}}; dayTwoDigit:{{dayTwoDigit}}; hourTwoDigit:{{hourTwoDigit}}; hour12:{{hour12}}; minuteTwoDigit:{{minuteTwoDigit}}; secondTwoDigit:{{secondTwoDigit}}; dayPeriod:{{dayPeriod}};
+      </div>
+      </template>
+  </mip-date-display>
+```
---
+++ components/mip-date-display/mip-date-display.js
@@ -0,0 +1,263 @@
+/**
+ * @file mip-date-display.js 日期显示组件
+ * @author panwenshuang
+ *
+ */
+
+let {
+    CustomElement,
+    templates
+  } = MIP
+
+/** @const {string} */
+const DEFAULT_LOCALE = 'zh-cn'
+
+/** @const {number} */
+const DEFAULT_OFFSET_SECONDS = 0
+
+/** @typedef {{
+  year: number,
+  month: number,
+  monthName: string,
+  monthNameShort: string,
+  day: number,
+  dayName: string,
+  dayNameShort: string,
+  hour: number,
+  minute: number,
+  second: number,
+  iso: string,
+}} */
+let VariablesDef;
+
+/** @typedef {{
+  year: number,
+  month: number,
+  monthName: string,
+  monthNameShort: string,
+  day: number,
+  dayName: string,
+  dayNameShort: string,
+  hour: number,
+  minute: number,
+  second: number,
+  iso: string,
+  yearTwoDigit: string,
+  monthTwoDigit: string,
+  dayTwoDigit: string,
+  hourTwoDigit: string,
+  hour12: string,
+  hour12TwoDigit: string,
+  minuteTwoDigit: string,
+  secondTwoDigit: string,
+  dayPeriod: string,
+ }} */
+let EnhancedVariablesDef;
+
+export default class MIPDateDisplay extends CustomElement {
+  build () {
+    this.container = this.element.ownerDocument.createElement('div')
+    this.element.appendChild(this.container)
+    
+    // Note: One of datetime, timestamp-ms, timestamp-seconds is required.
+    this.datetime = this.element.getAttribute('datetime') || ''
+    
+    this.timestampSeconds = Number(this.element.getAttribute('timestamp-seconds'))
+    
+    this.timestampMiliseconds = Number(this.element.getAttribute('timestamp-ms'))
+ 
+    this.displayIn = this.element.getAttribute('display-in') || ''
+ 
+    this.offsetSeconds =
+      Number(this.element.getAttribute('offset-seconds')) ||
+      DEFAULT_OFFSET_SECONDS
+
+    this.locale = this.element.getAttribute('locale') || DEFAULT_LOCALE
+
+    const data = this.getDataForTemplate()
+    
+    this.renderTemplate(data)
+  }
+
+   /**
+   * renderTemplate 获取模版
+   *
+   * @param {Object} data 渲染数据
+   */
+  renderTemplate (data) {
+    if (data) {
+      templates.render(this.element, data).then(render.bind(this))
+    } else {
+      console.error('数据不符合规范')
+    }
+  }
+
+  /**
+   * get data for template 获取模板数据
+   * 
+   * @return {!EnhancedVariablesDef}
+   */
+  getDataForTemplate() {
+    const targetTime = this.getTargetTime();
+    const offset = this.offsetSeconds * 1000;
+    const date = new Date(targetTime + offset);
+    const inUTC = this.displayIn.toLowerCase() === 'utc';
+    const basicData = inUTC
+      ? this.getVariablesInUTC(date, this.locale)
+      : this.getVariablesInLocal(date, this.locale);
+
+    return this.enhanceBasicVariables(basicData);
+  }
+
+   /**
+   * getTargetTime 获取目标时间
+   * 
+   * @return {number|undefined}
+   * @private
+   */
+  getTargetTime () {
+    let targetTime
+
+    if (this.datetime.toLowerCase() === 'now') {
+      targetTime = Date.now();
+    } else if (this.datetime) {
+      targetTime = Date.parse(this.datetime)
+    } else if (this.timestampMiliseconds) {
+      targetTime = +this.timestampMiliseconds;
+    } else if (this.timestampSeconds) {
+      targetTime = this.timestampSeconds * 1000;
+    }
+
+    if (targetTime === undefined) {
+      console.log('One of datetime, timestamp-ms, or timestamp-seconds is required')
+    }
+
+    return targetTime
+  }
+
+   /**
+   * get variables in utc 获取utc下的时间数据
+   * 
+   * @param {!Date} date
+   * @param {string} locale
+   * @return {!VariablesDef}
+   * @private
+   */
+  getVariablesInUTC(date, locale) {
+    return {
+      year: date.getUTCFullYear(),
+      month: date.getUTCMonth() + 1,
+      monthName: date.toLocaleDateString(locale, {
+        month: 'long',
+        timeZone: 'UTC',
+      }),
+      monthNameShort: date.toLocaleDateString(locale, {
+        month: 'short',
+        timeZone: 'UTC',
+      }),
+      day: date.getUTCDate(),
+      dayName: date.toLocaleDateString(locale, {
+        weekday: 'long',
+        timeZone: 'UTC',
+      }),
+      dayNameShort: date.toLocaleDateString(locale, {
+        weekday: 'short',
+        timeZone: 'UTC',
+      }),
+      hour: date.getUTCHours(),
+      minute: date.getUTCMinutes(),
+      second: date.getUTCSeconds(),
+      iso: date.toISOString(),
+    }
+  }
+
+  /**
+   * get variables in local 获取本地区时间数据
+   * 
+   * @param {!Date} date
+   * @param {string} locale
+   * @return {!VariablesDef}
+   * @private
+   */
+  getVariablesInLocal(date, locale) {
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth() + 1,
+      monthName: date.toLocaleDateString(locale, {month: 'long'}),
+      monthNameShort: date.toLocaleDateString(locale, {
+        month: 'short',
+      }),
+      day: date.getDate(),
+      dayName: date.toLocaleDateString(locale, {weekday: 'long'}),
+      dayNameShort: date.toLocaleDateString(locale, {
+        weekday: 'short',
+      }),
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds(),
+      iso: date.toISOString(),
+    };
+  }
+
+  /**
+   * 获取附加时间数据，如12小时制下是时间等
+   * 
+   * @param {!VariablesDef} data
+   * @return {!EnhancedVariablesDef}
+   * @private
+   */
+  enhanceBasicVariables(data) {
+    const hour12 = data.hour % 12 || 12;
+
+    return /** @type {!EnhancedVariablesDef} */(
+      Object.assign({}, data, {
+        yearTwoDigit: this.padStart(data.year % 100),
+        monthTwoDigit: this.padStart(data.month),
+        dayTwoDigit: this.padStart(data.day),
+        hourTwoDigit: this.padStart(data.hour),
+        hour12,
+        hour12TwoDigit: this.padStart(hour12),
+        minuteTwoDigit: this.padStart(data.minute),
+        secondTwoDigit: this.padStart(data.second),
+        dayPeriod: data.hour < 12 ? 'am' : 'pm',
+      }));
+  }
+
+  /**
+   * pad start
+   *
+   * @param {number} input input
+   * @returns {string} pad string
+   * @private
+   */
+  padStart (input) {
+    if (input > 9) {
+      return input.toString()
+    }
+
+    return '0' + input
+  }
+}
+
+/**
+ * render dom 渲染函数
+ *
+ * @param {Array} htmls html对象数组
+ */
+function render (htmls) {
+  removeChildren(this.container)
+  let node = document.createElement('div')
+  node.innerHTML = htmls
+  this.container.appendChild(node)
+}
+
+/**
+ * remove children 删除子元素
+ * 
+ * @param {!Element} element
+ */
+function removeChildren (parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+}
