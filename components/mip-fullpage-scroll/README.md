1084c9e68520d94fb865616f92bd9e4c9fc5d6f7
DESKTOP-AKNTDO1\Wpx <ianmandy@163.com>
2019-05-09 15:08:18 +0800

    feat:修改 mip-fullpage-scroll 组件，新增导航点和切换方法

--- components/mip-fullpage-scroll/README.md
+++ components/mip-fullpage-scroll/README.md
@@ -14,10 +14,83 @@
 
 ## 示例
 
-示例说明
+基本使用方法
 
 ```html
-<mip-fullpage-scroll changefun="lr">
+<style mip-custom>
+  /* 针对导航点的样式配置 */
+  .navdots{
+    position: absolute;
+    bottom: 10%;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+  .navdot{
+    height: 20px;
+    width: 20px;
+    border: 1px solid black;
+    border-radius: 10px;
+    float: left;
+  }
+</style>
+<mip-fullpage-scroll changefun="lr" id="c-test">
+    <div id="pagebox">
+      <div class="section">页面1</div>
+      <div class="section">页面2</div>
+    </div>
+</mip-fullpage-scroll>
+```
+
+暴露方法
+ChangeTo(pageIndex)             快速切换页面到pageIndex（从0开始）
+MoveTo({top:...,left:...})      整体显示区域移动到位置（position:relative）
+BackToDefault()                 返回整体区域默认显示位置（0,0）
+
+
+```html
+<style mip-custom>
+  /* 针对导航点的样式配置 */
+  .navdots{
+    position: absolute;
+    bottom: 10%;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+  }
+  .navdot{
+    height: 20px;
+    width: 20px;
+    border: 1px solid black;
+    border-radius: 10px;
+    float: left;
+  }
+
+  /* 页面整体导航样式 */
+  .mainmenu{
+    background: black;
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    z-index: 100;
+  }
+  .backmenu{
+    background: black;
+    color: white;
+    padding: 20px;
+    border-radius: 10px;
+    position: absolute;
+    right: 10px;
+    top: 100px;
+    z-index: 100;
+  }
+</style>
+<div class="mainmenu" on="tap:c-test.MoveTo({top:0,left:'-100px'})"> menu </div>
+<div class="backmenu" on="tap:c-test.BackToDefault"> back(请点击menu后使用观察效果) </div>
+<mip-fullpage-scroll id="c-test">
     <div id="pagebox">
       <div class="section">页面1</div>
       <div class="section">页面2</div>
@@ -41,6 +114,16 @@ lr ： （left and right） 左右切换
 
 **默认值**：ud
 
+### id
+
+**说明**：
+用于确定组件标签，以提供方法使用。
+
+**必选项**：是
+
+**单位**：无
+
+**默认值**：ud
 
 
 
@@ -59,3 +142,29 @@ lr ： （left and right） 左右切换
 用于包裹单一页面内容，整体显示内容在其中，可存在多个
 
 **必选项**：是
+
+
+
+## 方法
+
+### ChangeTo(pageIndex)
+
+**说明**：
+快速切换页面到pageIndex（从0开始）
+
+**参数说明**：
+pageIndex   必选    页面编号，从0开始
+
+### MoveTo({top:...,left:...})
+
+**说明**：
+整体显示区域移动到位置（position:relative）
+
+**参数说明**：
+left        必选   页面移动目标点x轴方向位置
+top         必选   页面移动目标点y轴方向位置
+
+### BackToDefault()
+
+**说明**：
+返回整体区域默认显示位置（0,0）,需搭配MoveTo方法使用
--- components/mip-fullpage-scroll/mip-fullpage-scroll.js
+++ components/mip-fullpage-scroll/mip-fullpage-scroll.js
@@ -1,53 +1,97 @@
 import './index.less'
 
-export default class MIPFullpageScroll extends MIP.CustomElement {
+const {
+  CustomElement,
+  util,
+  viewer,
+  viewport
+} = MIP
+const Gesture = util.Gesture
+
+export default class MIPFullpageScroll extends CustomElement {
+
+  // 初始化
+  connectedCallback () {
+
+    // 引入API
+    this.gesture = new Gesture(this.element)
+
+    // 获取标签参数
+    this.pagebox = document.getElementById('pagebox')
+    this.allpages = this.element.getElementsByClassName('section').length
+    this.changefun = this.element.getAttribute('changefun') == undefined ? 'ud' : this.element.getAttribute('changefun')
+
+    // 关于外框尺寸
+    // this.unit = this.element.getAttribute('unit') == undefined ? 'v' : this.element.getAttribute('unit')
+    // this.width = this.element.getAttribute('width') == undefined ? '100' : this.element.getAttribute('width')
+    // this.height = this.element.getAttribute('height') == undefined ? '100' : this.element.getAttribute('height')
+
+    // console.log(this.unit,this.width,this.height)
+
+    // 初始化动画CSS类（待修改，可自定义动画效果）
+    this.pagebox.classList.add('ChangeAnimate')
+
+    // 初始化切换方式(待修改)
+    if (this.changefun === 'lr') {
+      this.pagebox.classList.add('lr')
+      this.pageboxwidth = 'width:' + this.allpages * 100 + 'vw;'
+    }
+    else{
+      this.pageboxwidth = ''
+    }
+
+    // 设定导航点
+    let mainid = this.element.getAttribute('id')
+    let indexDot = '<div class="navdots">'
+    for(let i = 1;i < this.allpages; i++){
+      indexDot += '<div class="navdot" on="tap:' + mainid + '.ChangeTo(' + ( i - 1 ) + ')"></div>'
+    }
+    indexDot += '</div>'
+    let index = util.dom.create(indexDot)
+    util.dom.insert(this.element,index)
+  }
+
+  // 切换函数
+  ChangeTo (event, index) {
+    if (this.changefun === 'ud') {
+      (e => e.style.cssText = 'top:-' + index * 100 + 'vh')(this.pagebox)
+    }
+    if (this.changefun === 'lr') {
+      (e => e.style.cssText = this.pageboxwidth + 'left:-' + index * 100 + 'vw;')(this.pagebox)
+    }
+  }
+
+  // 整体页面移动函数
+  MoveTo (event, opt) {
+    let option = util.jsonParse(opt)
+    this.element.style.cssText = 'top:' + option.top + ';left:' + option.left + ';'
+  }
+  BackToDefault () {
+    this.element.style.cssText = 'top:0;left:0;'
+  }
+
   build () {
-    let Gesture = MIP.util.Gesture
-    let gesture = new Gesture(this.element)
+    // 数据绑定
+    let mainclass = this
+    let allpages = this.allpages
 
-    // 获取全屏页面内容
-    let pagebox = document.getElementById('pagebox')
-    let allpages = this.element.getElementsByClassName('section').length
+    // 初始化应用变量
     let nowpage = 0
-    let pageboxwidth = 0
 
-    let changefun = this.element.getAttribute('changefun')
-    changefun = changefun === undefined ? 'lr' : 'ud'
-
-    if (changefun === 'lr') {
-      pagebox.classList.add('lr')
-      pageboxwidth = 'width:' + allpages * 100 + 'vw;'
-    }
-
-    let udSwipe = function (e) {
-      e.style.cssText = 'top:-' + nowpage * 100 + 'vh'
-    }
-    let lrSwipe = function (e) {
-      e.style.cssText = pageboxwidth + 'left:-' + nowpage * 100 + 'vw;'
-    }
-
-    pagebox.classList.add('ChangeAnimate')
-    gesture.on('swipe', function (event, data) {
-      if (changefun === 'ud') {
-        if (data.swipeDirection === 'up') {
+    // 滑动事件监听
+    this.gesture.on('swipe', function (event, data) {
+        if (data.swipeDirection === 'up' || data.swipeDirection === 'left') {
           if (nowpage < allpages - 1) nowpage++
-          udSwipe(pagebox)
         }
-        if (data.swipeDirection === 'down') {
+        if (data.swipeDirection === 'down' || data.swipeDirection === 'right') {
           if (nowpage > 0) nowpage--
-          udSwipe(pagebox)
         }
-      }
-      if (changefun === 'lr') {
-        if (data.swipeDirection === 'left') {
-          if (nowpage < allpages - 1) nowpage++
-          lrSwipe(pagebox)
-        }
-        if (data.swipeDirection === 'right') {
-          if (nowpage > 0) nowpage--
-          lrSwipe(pagebox)
-        }
-      }
+      mainclass.ChangeTo(event,nowpage)
     })
+
+    // 事件绑定
+    this.addEventAction('ChangeTo', this.ChangeTo.bind(this))
+    this.addEventAction('MoveTo', this.MoveTo.bind(this))
+    this.addEventAction('BackToDefault', this.BackToDefault.bind(this))
   }
 }
