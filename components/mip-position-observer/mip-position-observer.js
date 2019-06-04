b743b045db286ed80c5690cec03396c7a3b534a5
18369190717pan <270687594@qq.com>
2019-06-04 09:51:46 +0800

    rename

--- components/mip-position-observer/mip-position-observer.js
+++ components/mip-position-observer/mip-position-observer.js
@@ -1,12 +1,12 @@
-import {installPositionObserverService, POSITION_OBSERVER_SERVICE_ID} from './common/position-observer-service'
+import {installPositionObserverService, POSITION_OBSERVER_SERVICE_ID} from './service/position-observer-service'
 import {
   PositionObserverFidelity,
   PositionInViewportEntryDef,
   RelativePositions,
   layoutRectsRelativePos,
   LayoutRectDef
-} from './common/position-observer-worker'
-import {ScheduleService, SCHEDULE_SERVICE_ID} from './common/schedule-service'
+} from './service/position-observer-worker'
+import {ScheduleService, SCHEDULE_SERVICE_ID} from './service/schedule-service'
 const {CustomElement, util, viewer, viewport, Services} = MIP
 const rect = util.rect
 const RESIZE_THRESHOLD = 150
