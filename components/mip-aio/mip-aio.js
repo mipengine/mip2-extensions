f81d9998ae5fa79fe0ac4c65ca5643f4621fd51e
18369190717pan <270687594@qq.com>
2019-08-12 21:58:15 +0800

    change bad code

--- components/mip-aio/mip-aio.js
+++ components/mip-aio/mip-aio.js
@@ -1,18 +1,28 @@
+/**
+ * @file mip-aio 服务封装
+ * @author Miya(panwenshuang@baidu.com)
+ */
+
 const {registerService} = MIP
 
 export default class MIPAio {
   async getBox (version) {
-    return new Promise((resolve, reject) => {
+    const fileVersion = version || '201602'
+    if (this.scriptPromise) {
+      return this.scriptPromise
+    }
+    this.scriptPromise = new Promise((resolve, reject) => {
       const aioScript = document.createElement('script')
-      const srcUrl = 'http://s.bdstatic.com/common/openjs/aio.js?v=' + version
-      aioScript.src = srcUrl
+      const srcUrl = 'http://s.bdstatic.com/common/openjs/aio.js?v=' + fileVersion
       aioScript.id = 'bd-box-sdk'
-      document.head.appendChild(aioScript)
       aioScript.onload = () => resolve()
       aioScript.onerror = () => reject(new Error(`脚本加载失败: ${srcUrl}`))
+      aioScript.src = srcUrl
+      document.head.appendChild(aioScript)
     }).then(() => {
       return window.Box
     })
+    return this.scriptPromise
   }
 }
 
